9d2841f584e256e2595ee01dbad522d1a04e96a6
cioul <icoul@naver.com>
2018-03-20 20:09:28 +0900

    django form null value exception

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from .models import Contents
 import datetime
 
-DATE_INPUT_FORMATS = ['%Y%m%d%H%M%S']
+DATE_INPUT_FORMATS = ['%Y%m%d%H%M%S%f']
 
 class UserForm(ModelForm):
     class Meta:
@@ -22,7 +22,8 @@ class ContentsForm(ModelForm):
     contents      = forms.CharField(widget=forms.Textarea(attrs={'v-model': 'firewood_id', '@keydown.enter': 'insert_content'}))
     mention_index = forms.CharField(widget=forms.HiddenInput, initial='0')
     mention_order = forms.CharField(widget=forms.HiddenInput, initial='0')
-    image         = forms.FileField(widget=forms.FileInput(attrs={'accept': '.jpg, .jpeg, .png', '@change': 'onChangeImg'}), initial='')
+    image         = forms.FileField(widget=forms.FileInput(attrs={'accept': '.jpg, .jpeg, .png', '@change': 'onChangeImg'})\
+                    , initial='', required=False)
 
     class Meta:
         model = Contents
